3ac7c8fcc57cd1d60c4256ff285939cdccc1021e
foptun <oixuier.toppfun@gmail.com>
2022-01-20 21:21:49 +0700

    Create Ex01.cpp

---
+++ Ex01.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    
+    struct person{
+        int id;
+        char firstName[20];
+        char lastName[20];
+        char phone[10];
+        int salary;
+    };
+    
+    struct person p1;
+    
+    p1.id = 1;
+    strcpy(p1.firstName, "Phansa");
+    strcpy(p1.lastName, "Kanom");
+    strcpy(p1.phone, "0801234567");
+    p1.salary = 22000;
+    
+    printf("ID: %d \n", p1.id);
+    printf("First Name: %s \n", p1.firstName);
+    printf("Last Name: %s \n", p1.lastName);
+    printf("Phone: %s \n", p1.phone);
+    printf("Salary: %d \n", p1.salary);
+    
+    return 0;
+}
